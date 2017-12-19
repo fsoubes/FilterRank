bad5cad9ae5af51b94489535287737b6829dcb8f
aardvarkar <32220401+aardvarkar@users.noreply.github.com>
2017-12-19 01:32:25 +0100

    Add files via upload

--- examples/test_median.js
+++ examples/test_median.js
@@ -2,14 +2,20 @@
 
 
 let img0 = new T.Image('uint8',256,254);
+//let img0 = new T.Image('uint8',5760,4608);
 img0.setPixels(blobs_pixels);
+//img0.setPixels(boats_5760x4608);
 let raster0 = img0.getRaster();
 let img01 = new T.Image('uint16',256,254);
+//let img01 = new T.Image('uint16',5760,4608);
 let uint16_blobs = blobs_pixels.map ( (px) => px * 256);
+//let uint16_blobs = boats_5760x4608.map ( (px) => px * 256);
 img01.setPixels(uint16_blobs);
 let raster01 = img01.getRaster();
 let img02 = new T.Image('float32',256,254);
+//let img02 = new T.Image('float32',5760,4608);
 let float_blobs = blobs_pixels.map( (px) => px/128 - 1.0);
+//let float_blobs = boats_5760x4608.map( (px) => px/128 - 1.0);
 img02.setPixels(float_blobs);
 let raster02 = img02.getRaster();
 let win02 = new T.Window('Blobs float32');
