c1c1f6028244c161f144740e6ab01c2845657fb4
fsoubes <franck.soubes@hotmail.fr>
2017-12-20 23:35:47 +0100

    Update test_variancefilter.js

--- examples/test_variancefilter.js
+++ examples/test_variancefilter.js
@@ -16,9 +16,7 @@ let kernel = new T.Raster('uint8',2,2);
 let kernel_size = kernel.width;
 
 
-//let img0 = new T.Image('uint8',360,288);
 let img1 = new T.Image('uint8',256,254);
-//img0.setPixels(boats_pixels);
 img1.setPixels(pixels);
 let win1 = new T.Window('Blobs 8 bit');
 let view1 = T.view(img1.getRaster());
@@ -28,19 +26,14 @@ win1.addToDOM('workspace');
 
 
 let img2 = new T.Image('uint8',256,254);
-//let ImgI = variance(img0,img111,kernel_size);
 img2.setPixels(blobs_pixels);
 let win2 = new T.Window('Blob 8bit cropped');
-
 let workflow = T.pipe(variance(kernel),T.view);
-
 let view2 = workflow(img2.getRaster());
 let crop8 = T.pipe(T.crop(1,2,view2.width - kernel_size, view2.height - kernel_size), T.view);
 let view3 = crop8(img2.getRaster());
 win2.addView(view2);
 win2.addToDOM('workspace');
-//let img2 = new T.Image('uint8',360,288);
-
 
 
 /**
@@ -51,18 +44,14 @@ let img01 = new T.Image('uint16',256,254);
 img01.setPixels(uint16_blobs);
 let win01 = new T.Window('Blobs uint16 cropped');
 let view01 = T.view(img01.getRaster());
-// Create the window content from the view
 win01.addView(view01);
-// Add the window to the DOM and display it
 win01.addToDOM('workspace');
 
 
 let img02 = new T.Image('uint16',256,254);
 img02.setPixels(uint16_blobs);
 let win02 = new T.Window('Blobs uint16');
-
 let workflow1 = T.pipe(variance(kernel),T.view);
-
 let view02 = workflow1(img02.getRaster());
 let cropp16 = T.pipe(T.crop(1,2,view02.width - kernel_size, view02.height - kernel_size), T.view);
 let view03 = cropp16(img02.getRaster());
@@ -77,13 +66,10 @@ win02.addToDOM('workspace');
 
 
 let img001 = new T.Image('float32',256,254);
-
 img001.setPixels(float_blobs);
 let win001 = new T.Window('Blobs float32');
 let view001 = T.view(img001.getRaster());
-// Create the window content from the view
 win001.addView(view001);
-// Add the window to the DOM and display it
 win001.addToDOM('workspace');
 
 
@@ -92,11 +78,8 @@ win001.addToDOM('workspace');
 let img002 = new T.Image('float32',256,254);
 img002.setPixels(float_blobs);
 let win002 = new T.Window('Blobs float32');
-
 let workflow2 = T.pipe(variance(kernel),T.view);
-
 let view002 = workflow2(img002.getRaster());
-
 win002.addView(view002);
 win002.addToDOM('workspace');
 
