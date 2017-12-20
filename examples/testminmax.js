d7c1b115e74ecfbde89c18c2c995c44b35e93829
Hiltarius <debrasguillaume@yahoo.fr>
2017-12-20 13:34:05 +0100

    Add files via upload

--- examples/testminmax.js
+++ examples/testminmax.js
@@ -2,16 +2,17 @@ const max = 'max';
 const min = 'min';
 
 // image de base
-let img0 = new T.Image('uint8',360,280);
-img0.setPixels(boats_pixels);
+let img0 = new T.Image('uint8',256,254);
+img0.setPixels(blobs_pixels);
 let win0 = new T.Window('Blob 8bit');
 let view0 = T.view(img0.getRaster());
 win0.addView(view0);
 win0.addToDOM('workspace');
 
-let filtre8 =min_max(img0,5,max);
+//function min_max 
+let filtre8 =min_max(img0,7,max);
 
-let img8 = new T.Image('uint8',360,280);
+let img8 = new T.Image('uint8',256,254);
 img8.setPixels(filtre8);
 let win8 = new T.Window('Blob min_max filter 8 bit');
 let view8 = T.view(img8.getRaster());
@@ -32,7 +33,9 @@ win01.addView(view01);
 // Add the window to the DOM and display it
 win01.addToDOM('workspace');
 
-let filtre16 =min_max(img01,5,max);
+// function min_max
+let filtre16 =min_max(img01,9,max);
+
 
 let img32 = new T.Image('uint16',256,254);
 img32.setPixels(filtre16);
@@ -44,20 +47,19 @@ win12.addToDOM('workspace');
 
  // Display float32 images
  
-let img02 = new T.Image('float32',256,254);
-let float_blobs = blobs_pixels.map( (px) => px/128 - 1.0);
-img02.setPixels(float_blobs);
+let img03 = new T.Image('float32',256,254);
+let float_boats = blobs_pixels.map( (px) => px/128 - 1.0);
+img03.setPixels(float_boats);
 let win02 = new T.Window('Blobs float32');
-let view02 = T.view(img02.getRaster());
+let view02 = T.view(img03.getRaster());
 // Create the window content from the view
 win02.addView(view02);
 // Add the window to the DOM and display it
 win02.addToDOM('workspace');
 
 
-
-let filtre2 =min_max(img02,5,max);
-
+// function min_max
+let filtre2 =min_max(img03,11,min);
 
 
 
