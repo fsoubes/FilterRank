db8968e46f21dc8447256279ea0e08774a832eaa
aardvarkar <32220401+aardvarkar@users.noreply.github.com>
2017-12-21 14:54:56 +0100

    Add files via upload

--- examples/test_median.js
+++ examples/test_median.js
@@ -1,21 +1,23 @@
 "use strict";
 
-
-let img0 = new T.Image('uint8',256,254);
-//let img0 = new T.Image('uint8',5760,4608);
-img0.setPixels(blobs_pixels);
-//img0.setPixels(boats_5760x4608);
+let sizeWidth = 900;
+let sizeHeight = 720;
+let sampleName = boats_900x720;
+//let img0 = new T.Image('uint8',256,254);
+let img0 = new T.Image('uint8',sizeWidth,sizeHeight);
+//img0.setPixels(blobs_pixels);
+img0.setPixels(sampleName);
 let raster0 = img0.getRaster();
-let img01 = new T.Image('uint16',256,254);
-//let img01 = new T.Image('uint16',5760,4608);
-let uint16_blobs = blobs_pixels.map ( (px) => px * 256);
-//let uint16_blobs = boats_5760x4608.map ( (px) => px * 256);
+//let img01 = new T.Image('uint16',256,254);
+let img01 = new T.Image('uint16',sizeWidth,sizeHeight);
+//let uint16_blobs = blobs_pixels.map ( (px) => px * 256);
+let uint16_blobs = sampleName.map ( (px) => px * 256);
 img01.setPixels(uint16_blobs);
 let raster01 = img01.getRaster();
-let img02 = new T.Image('float32',256,254);
-//let img02 = new T.Image('float32',5760,4608);
-let float_blobs = blobs_pixels.map( (px) => px/128 - 1.0);
-//let float_blobs = boats_5760x4608.map( (px) => px/128 - 1.0);
+//let img02 = new T.Image('float32',256,254);
+let img02 = new T.Image('float32',sizeWidth,sizeHeight);
+//let float_blobs = blobs_pixels.map( (px) => px/128 - 1.0);
+let float_blobs = sampleName.map( (px) => px/128 - 1.0);
 img02.setPixels(float_blobs);
 let raster02 = img02.getRaster();
 let win02 = new T.Window('Blobs float32');
@@ -27,16 +29,35 @@ win02.addToDOM('workspace');
 console.log(img02.raster.pixelData);
 
 //let kernelSize = 11;
-let kernel = new T.Raster('uint8',11,11);
+let kernel = new T.Raster('uint8',2,2);
 
+let nbTest=1;
 
-
-/*var t0 = performance.now();
-for (var i = 0; i < 1; i++) {
-  medianFilter(kernel)(raster0);
+var t0 = performance.now();
+for (var i = 0; i < nbTest; i++) {
+    medianFilter(kernel)(raster0);
 }
 var t1 = performance.now();
-console.log('Took', ((t1 - t0) / 10).toFixed(4), 'milliseconds to generate');*/
+console.log('8 bit');
+console.log('Took', ((t1 - t0) / nbTest).toFixed(4), 'milliseconds to generate');
+
+var t0 = performance.now();
+for (var i = 0; i < nbTest; i++) {
+    medianFilter(kernel)(raster01);
+}
+var t1 = performance.now();
+console.log('16 bit');
+console.log('Took', ((t1 - t0) / nbTest).toFixed(4), 'milliseconds to generate');
+
+var t0 = performance.now();
+for (var i = 0; i < nbTest; i++) {
+    medianFilter(kernel)(raster02);
+}
+var t1 = performance.now();
+console.log('float 32');
+console.log('Took', ((t1 - t0) / nbTest).toFixed(4), 'milliseconds to generate');
+
+
 let medianFilteredImg0 = medianFilter(kernel)(raster0);
 let win10 = new T.Window('New Blobs');
 let view10 = T.view(medianFilteredImg0);
