d5dd00bd99f5024f81a67255c27a13265c660ad4
franck soubes <franck1337@Sagan>
2017-12-23 14:02:03 +0100

    yesterdaywasnotpushing

--- examples/TestrankFilters.js
+++ examples/TestrankFilters.js
@@ -1,59 +1,39 @@
+
 "use strict";
 
 let sizeWidth = 360;
 let sizeHeight = 288;
 let sampleName = boats_pixels;
-//let img0 = new T.Image('uint8',256,254);
-let img0 = new T.Image('uint8',sizeWidth,sizeHeight);
-//img0.setPixels(blobs_pixels);
-img0.setPixels(sampleName);
-let raster0 = img0.getRaster();
-//let img01 = new T.Image('uint16',256,254);
-let img01 = new T.Image('uint16',sizeWidth,sizeHeight);
-//let uint16_blobs = blobs_pixels.map ( (px) => px * 256);
-let uint16_blobs = sampleName.map ( (px) => px * 256);
-img01.setPixels(uint16_blobs);
-let raster01 = img01.getRaster();
-//let img02 = new T.Image('float32',256,254);
-let img02 = new T.Image('float32',sizeWidth,sizeHeight);
-//let float_blobs = blobs_pixels.map( (px) => px/128 - 1.0);
-let float_blobs = sampleName.map( (px) => px/128 - 1.0);
-img02.setPixels(float_blobs);
-let raster02 = img02.getRaster();
 
-let win0 = new T.Window('image');
-let view0 = T.view(img0.getRaster());
+//let img0 = new T.Image('uint8',256,254);
+let img00 = new T.Image('uint8',sizeWidth,sizeHeight);
+//img0.setPixels(blobs_pixels);
+img00.setPixels(sampleName);
+let raster0 = img00.getRaster();
+//let img01 = new T.Image('uint16',256,254);
+let img0120 = new T.Image('uint16',sizeWidth,sizeHeight);
+//let uint16_blobs = blobs_pixels.map ( (px) => px * 256);
+let uint16_blobss = sampleName.map ( (px) => px * 256);
+img0120.setPixels(uint16_blobss);
+let raster01 = img0120.getRaster();
+//let img02 = new T.Image('float32',256,254);
+let img0220 = new T.Image('float32',sizeWidth,sizeHeight);
+//let float_blobs = blobs_pixels.map( (px) => px/128 - 1.0);
+let float_blobss = sampleName.map( (px) => px/128 - 1.0);
+img0220.setPixels(float_blobss);
+let raster02 = img0220.getRaster();
+
+let win00 = new T.Window('image');
+let view00 = T.view(img00.getRaster());
 // Create the window content from the view
-win0.addView(view0);
+win00.addView(view00);
 // Add the window to the DOM and display it
-win0.addToDOM('workspace');
+win00.addToDOM('workspace');
 
 //let kernelSize = 11;
 let kernel = new T.Raster('uint8',3,3);
 
-/*
-let nbTest=100;
-var t0 = performance.now();
-for (var i = 0; i < nbTest; i++) {
-    medianFilter(kernel)(raster0);
-}
-var t1 = performance.now();
-console.log('8 bit');
-console.log('Took', ((t1 - t0) / nbTest).toFixed(4), 'milliseconds to generate');
-var t0 = performance.now();
-for (var i = 0; i < nbTest; i++) {
-    medianFilter(kernel)(raster01);
-}
-var t1 = performance.now();
-console.log('16 bit');
-console.log('Took', ((t1 - t0) / nbTest).toFixed(4), 'milliseconds to generate');
-var t0 = performance.now();
-for (var i = 0; i < nbTest; i++) {
-    medianFilter(kernel)(raster02);
-}
-var t1 = performance.now();
-console.log('float 32');
-console.log('Took', ((t1 - t0) / nbTest).toFixed(4), 'milliseconds to generate');*/
+
 
 
 let medianFilteredImg0 = medianFilter(kernel)(raster0);
@@ -73,6 +53,8 @@ win20.addView(view20);
 win30.addView(view30);
 win20.addToDOM('workspace');
 win30.addToDOM('workspace');
+
+
 
 
 const max = 'max';
@@ -125,8 +107,8 @@ win12.addToDOM('workspace');
  // Display float32 images
  
 let img03 = new T.Image('float32',256,254);
-let float_boats = blobs_pixels.map( (px) => px/128 - 1.0);
-img03.setPixels(float_boats);
+let float_blobs = blobs_pixels.map( (px) => px/128 - 1.0);
+img03.setPixels(float_blobs);
 let win02 = new T.Window('Blobs float32');
 let view02 = T.view(img03.getRaster());
 // Create the window content from the view
@@ -156,20 +138,17 @@ win2.addToDOM('workspace');
 ////////////////////////////////////////
 /////////// Variance_filter ////////////
 ////////////////////////////////////////
-"use strict";
 
-/**
- * Display uint8 images
- */
+// Display uint8 images/
 
 
 let width_size = 360;
 let height_size = 288;
 let pixels = boats_pixels;
-let uint16_blobs = pixels.map ((px) => px * 256);
-let float_blobs = pixels.map( (px) => px/128 - 1.0);
-let kernel = new T.Raster('uint8',2,2);
-let kernel_size = kernel.width;
+let uint16_boats = pixels.map ((px) => px * 256);
+let float_boats = pixels.map( (px) => px/128 - 1.0);
+let kernel1 = new T.Raster('uint8',2,2);
+let kernel_size = kernel1.width;
 
 
 let img1 = new T.Image('uint8',width_size,height_size);
@@ -180,38 +159,37 @@ win1.addView(view1);
 win1.addToDOM('workspace');
 
 
-let img2 = new T.Image('uint8',width_size,height_size);
-img2.setPixels(pixels);
-let win2 = new T.Window('Boats 8bit with variance filter croped');
+let img2222 = new T.Image('uint8',width_size,height_size);
+img2222.setPixels(pixels);
+let win2222 = new T.Window('Boats 8bit with variance filter croped');
 let workflow = T.pipe(variance(kernel),T.view);
-let view2 = workflow(img2.getRaster());
-let crop8 = T.pipe(T.crop(1,2,view2.width - kernel_size, view2.height - kernel_size), T.view);
+let view2222 = workflow(img2222.getRaster());
+let crop8 = T.pipe(T.crop(1,2,view2222.width - kernel_size, view2222.height - kernel_size), T.view);
 crop8(img2.getRaster());
-win2.addView(view2);
-win2.addToDOM('workspace');
+win2222.addView(view2222);
+win2222.addToDOM('workspace');
 
 
-/**
- * Display uint16 images
- */
+// Display uint16 images
+ 
 
-let img01 = new T.Image('uint16',width_size,height_size);
-img01.setPixels(uint16_blobs);
-let win01 = new T.Window('Boats uint16 ');
-let view01 = T.view(img01.getRaster());
-win01.addView(view01);
-win01.addToDOM('workspace');
+let img1111 = new T.Image('uint16',width_size,height_size);
+img1111.setPixels(uint16_boats);
+let win01111 = new T.Window('Boats uint16 ');
+let view01111 = T.view(img1111.getRaster());
+win01111.addView(view01111);
+win01111.addToDOM('workspace');
 
 
 let img02 = new T.Image('uint16',width_size,height_size);
-img02.setPixels(uint16_blobs);
-let win02 = new T.Window('Boats uint16  with variance filter croped');
+img02.setPixels(uint16_boats);
+let win02222 = new T.Window('Boats uint16  with variance filter croped');
 let workflow1 = T.pipe(variance(kernel),T.view);
-let view02 = workflow1(img02.getRaster());
-let cropp16 = T.pipe(T.crop(1,2,view02.width - kernel_size, view02.height - kernel_size), T.view);
+let view02222 = workflow1(img02.getRaster());
+let cropp16 = T.pipe(T.crop(1,2,view02222.width - kernel_size, view02222.height - kernel_size), T.view);
 cropp16(img02.getRaster());
-win02.addView(view02);
-win02.addToDOM('workspace');
+win02222.addView(view02222);
+win02222.addToDOM('workspace');
 
  
 
@@ -220,7 +198,7 @@ win02.addToDOM('workspace');
 
 
 let img001 = new T.Image('float32',width_size,height_size);
-img001.setPixels(float_blobs);
+img001.setPixels(float_boats);
 let win001 = new T.Window('Blobs float32');
 let view001 = T.view(img001.getRaster());
 win001.addView(view001);
@@ -230,9 +208,10 @@ win001.addToDOM('workspace');
 // can't crop float32
 
 let img002 = new T.Image('float32',width_size,height_size);
-img002.setPixels(float_blobs);
+img002.setPixels(float_boats);
 let win002 = new T.Window('Blobs float32 with variance filter');
 let workflow2 = T.pipe(variance(kernel),T.view);
 let view002 = workflow2(img002.getRaster());
 win002.addView(view002);
 win002.addToDOM('workspace');
+
