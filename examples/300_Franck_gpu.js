ca93f492faec0ace34180ca34bc1c79dce880bdc
root <root@Sagan.Sagan>
2018-02-01 16:18:54 +0100

    fsloop

--- examples/300_Franck_gpu.js
+++ examples/300_Franck_gpu.js
@@ -10,7 +10,7 @@ let img = new T.Image('uint8',360,288);
 img.setPixels(new Uint8Array(boats_pixels));
 
 // Run CPU mean 5x5 
-let size = 3;
+let size = 5;
 let radius = size / 2.0 - 0.5;
 let kernel5x5 = cpu.convolutionKernel(
  //cpu.CPU_HARDWARE,                            // For cpu.convolve
--- src/gpu/gpu_Franck.js
+++ src/gpu/gpu_Franck.js
@@ -33,62 +33,75 @@ https://medium.com/david-guan/webgl-and-image-filter-101-5017b290d02f
  */
 
 const varianceFilter = (raster, graphContext, kernel, copy_mode = true) => {
-    
-    let varTest=0.5;
+
+    //Create GPU kernel
+    let sizeKernel = kernel.length;
+    let horizontalOffset = kernel.map((i) => i.offsetX);
+    let verticalOffset = kernel.map((i) => i.offsetY);
+
 
     let id='Franck';
-    
+
     let src_vs = `#version 300 es
 
-	in vec2 a_vertex;
-	in vec2 a_texCoord;
-    
+  	in vec2 a_vertex;
+  	in vec2 a_texCoord;
+
     uniform vec2 u_resolution;
-    
+
     out vec2 v_texCoord;
-    
+
     void main() {
-	v_texCoord = a_texCoord;
-	vec2 clipSpace = a_vertex * u_resolution * 2.0 - 1.0;
-	gl_Position = vec4( clipSpace * vec2(1,-1), 0.0, 1.0);
+
+  	v_texCoord = a_texCoord;
+  	vec2 clipSpace = a_vertex * u_resolution * 2.0 - 1.0;
+  	gl_Position = vec4( clipSpace * vec2(1,-1), 0.0, 1.0);
+
     }`;
 
     let src_fs = `#version 300 es
     precision mediump float;
-    
-	in vec2 v_texCoord;
+
+	  in vec2 v_texCoord;
     uniform sampler2D u_image;
+    uniform float u_horizontalOffset[13];
+    uniform float u_verticalOffset[13];
+
 
     out vec4 outColor;
-    
+
     void main() {
-	
-	outColor = vec4(1.0 - texture(u_image, v_texCoord).rgb, 1.0); 
+      float testArray[13];
+      vec3 kernelContent[13];
+
+    for(int i = 0 ; i<13 ; i=i+1) {
+      kernelContent[i] = texture(u_image, vec2(v_texCoord.x + u_horizontalOffset[i] , v_texCoord.y + u_verticalOffset[i])).rgb;
+    }
+    vec3 testVec = texture(u_image, vec2(v_texCoord.x + u_horizontalOffset[0] , v_texCoord.y + u_verticalOffset[0])).rgb;
+    outColor = vec4(testVec,1.0);
+	  //outColor = vec4(1.0 - texture(u_image, v_texCoord).rgb, 1.0);
     }`;
-    
+
 
     // Step #1: Create - compile + link - shader program
     let the_shader = gpu.createProgram(graphContext,src_vs,src_fs);
 
     console.log('programs done...');
-    
+
     // Step #2: Create a gpu.Processor, and define geometry, attributes, texture, VAO, .., and run
     let gproc = gpu.createGPU(graphContext)
-	.size(raster.width,raster.height)
-	.geometry(gpu.rectangle(raster.width,raster.height))
-	.attribute('a_vertex',2,'float', 16,0)      // X, Y
-	.attribute('a_texCoord',2, 'float', 16, 8)  // S, T
-	.texture(raster,0)
-	.packWith(the_shader) // VAO
-	.clearCanvas([0.0,1.0,1.0,1.0])
-	.preprocess()
-	.uniform('u_resolution',new Float32Array([1.0/raster.width,1.0/raster.height]))
-	.uniform('u_image',0)
-	.run();
-
-    
+	  .size(raster.width,raster.height)
+    .geometry(gpu.rectangle(raster.width,raster.height))
+	  .attribute('a_vertex',2,'float', 16,0)      // X, Y
+	  .attribute('a_texCoord',2, 'float', 16, 8)  // S, T
+	  .texture(raster,0)
+	  .packWith(the_shader) // VAO
+	  .clearCanvas([0.0,1.0,1.0,1.0])
+	  .preprocess()
+	  .uniform('u_resolution',new Float32Array([1.0/raster.width,1.0/raster.height]))
+    .uniform('u_image',0)
+	  .uniform('u_horizontalOffset', horizontalOffset) //Ajout
+	  .uniform('u_verticalOffset', verticalOffset) //Ajout
+	  .run();
     return raster;
 }
-
-
-
